bd838e6349df4f4dfe69c22e6e4b017bdc745665
Praveen <praveen.velanati@sparity.com>
2019-06-22 21:30:51 -0400

    Initial Commit

---
+++ TopApps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TopApps
+//
+//  Created by praveen velanati on 6/22/19.
+//  Copyright © 2019 praveen velanati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TopAppsTests/TopAppsTests.swift
@@ -0,0 +1,34 @@
+//
+//  TopAppsTests.swift
+//  TopAppsTests
+//
+//  Created by praveen velanati on 6/22/19.
+//  Copyright © 2019 praveen velanati. All rights reserved.
+//
+
+import XCTest
+@testable import TopApps
+
+class TopAppsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
